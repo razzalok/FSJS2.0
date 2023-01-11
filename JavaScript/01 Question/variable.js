29214ad8edf211dd6f3fbcb02d1791042cae1caf
Alok Raj <alokraj7184@gmail.com>
2023-01-11 06:37:24 +0530

    issue resolved

---
+++ JavaScript/01 Question/variable.js
@@ -0,0 +1,1 @@
+// 01. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
---
+++ JavaScript/Practics/07-01-2023.js
@@ -0,0 +1,18 @@
+const object ={
+    rocket: '🚀',
+    home: '🏡'
+}
+
+const obj1 = {
+    rocket: '🚀',
+    fuel: 2,
+    config:{
+        name: 'mars'
+    }
+}
+
+obj1.fuel = 200
+obj1['year'] = 2000
+
+// part 2
+console.log(obj1);
