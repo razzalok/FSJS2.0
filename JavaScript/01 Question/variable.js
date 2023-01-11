a20ba9d620dd2f4fd32c71e763f0772f1a22d156
Alok Raj <alokraj7184@gmail.com>
2023-01-11 06:51:19 +0530

    issue resolved

--- JavaScript/01 Question/variable.js
+++ JavaScript/01 Question/variable.js
@@ -1,1 +1,3 @@
 // 01. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+const name ="Alok Raj"
+console.log(typeof(name));
