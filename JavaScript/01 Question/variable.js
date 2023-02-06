15cd758dd291aa88b8e63bc4aeaaa93cb5afebc0
Alok Raj <alokraj7184@gmail.com>
2023-02-06 19:10:45 +0530

    Some question solved

--- JavaScript/01 Question/variable.js
+++ JavaScript/01 Question/variable.js
@@ -1,3 +1,20 @@
 // 01. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+console.log(`String implementations`);
 const name ="Alok Raj"
+console.log(name);
 console.log(typeof(name));
+
+console.log(`Boolean implementation`);
+var isValid = true
+console.log(isValid);
+console.log(typeof isValid);
+
+console.log(`Undefined implementation`);
+let sum;
+console.log(sum);
+console.log(typeof sum);
+
+console.log(`Null implementation`);
+const payment = null;
+console.log(payment);
+console.log(typeof payment);
