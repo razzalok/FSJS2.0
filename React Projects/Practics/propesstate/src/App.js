eb049a033690a73e8bbd7fda5011bb98015ada6d
Alok Raj <alokraj7184@gmail.com>
2023-03-14 11:11:41 +0530

    dom project

--- React Projects/Practics/propesstate/src/App.js
+++ React Projects/Practics/propesstate/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
 
 function App() {
+  const [count,setCount]= useState(0)
+  const incrementCount= ()=>{
+    setCount(count+1);
+  }
+  const decrementCount = ()=>{
+    setCount(count-1);
+  }
+  const products = [
+    {title:"Alok",id:1},
+    {title:"Ankit",id:2},
+    {title:"Boss",id:3},
+  ]
+  const listItem = products.map( p =>
+    <li key={p.id}>{p.title}</li>
+    )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <div className="btn">
+        <button onClick={incrementCount}>+</button>
+        <button onClick={decrementCount}>-</button>
+      </div>
+      <ul>{listItem}</ul>
+      
     </div>
   );
 }
