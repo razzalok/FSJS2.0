1150598afbc46bfdfc042e683cd189c291cb73f9
Alok Raj <alokraj7184@gmail.com>
2023-03-15 06:07:52 +0530

    Dom Practics1

--- JavaScript/Dom Project/04_DOM Project/index.js
+++ JavaScript/Dom Project/04_DOM Project/index.js
@@ -1,3 +1,28 @@
-const card1 = document.querySelector('.clearfix');
-card1.style.backgroundColor = "#EC9B3B"
-card1.style.color = "#fff"
+const card1 = document.querySelectorAll('.clearfix');
+card1[0].style.backgroundColor = "#EC9B3B"
+card1[0].style.color = "#fff"
+console.log(card1);
+card1[1].style.backgroundColor = "#EE5487"
+card1[2].style.backgroundColor = "#F6901A"
+card1[3].style.backgroundColor = "#82BB31"
+card1[4].style.backgroundColor = "#4FACFF"
+
+const onethird = document.querySelectorAll(".one-third")
+console.log(onethird);
+// const onethirdArr=[...onethird]
+onethird[0].style.color="#fff"
+onethird[1].style.color="#fff"
+onethird[2].style.color="#fff"
+onethird[3].style.color="#fff"
+onethird[4].style.color="#fff"
+onethird[5].style.color="#fff"
+onethird[6].style.color="#fff"
+onethird[7].style.color="#fff"
+onethird[8].style.color="#fff"
+onethird[9].style.color="#fff"
+onethird[10].style.color="#fff"
+onethird[11].style.color="#fff"
+onethird[12].style.color="#fff"
+onethird[13].style.color="#fff"
+onethird[14].style.color="#fff"
+
