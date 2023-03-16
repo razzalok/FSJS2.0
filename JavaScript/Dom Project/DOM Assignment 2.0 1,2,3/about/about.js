35dd3b19b45b5882365bb65fc9eddf57788a89dc
razzalok <alokraj7184gmail.com>
2023-03-16 07:22:14 +0530

    Dom projects

--- JavaScript/Dom Project/DOM Assignment 2.0 1,2,3/about/about.js
+++ JavaScript/Dom Project/DOM Assignment 2.0 1,2,3/about/about.js
@@ -9,3 +9,25 @@ accordian.forEach((element) => {
     }
   });
 });
+accordian[2].nextElementSibling.style.display="block"
+accordian[3].nextElementSibling.style.display="block"
+
+const contactreplace = document.querySelectorAll("li")
+// contactreplace[2].innerText = "Projects"
+
+
+
+const accordianWrapper = document.querySelector(".accordian-wrapper")
+const newaccorian = document.createElement("div");
+newaccorian.classList.add("accordian");
+const h3 = document.createElement("h3");
+h3.textContent="Skills";
+const p=document.createElement("p");
+p.textContent="I posses a very good command over the Full Stack devlopment techno;ogies like MERN which can be seen in my work over the Github."
+
+newaccorian.appendChild(h3)
+newaccorian.appendChild(p)
+p.style.display="block"
+
+accordianWrapper.appendChild(newaccorian)
+
