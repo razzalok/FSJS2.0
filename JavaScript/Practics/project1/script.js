8636f3d45d3c528a8f3b41221a70eeaf597ef792
Alok Raj <alokraj7184@gmail.com>
2023-03-27 10:41:42 +0530

    Update script.js

--- JavaScript/Practics/project1/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("button").addEventListener("click",changebgc)
-function changebgc(){
-    let color = "#"+ Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor=color
-}
